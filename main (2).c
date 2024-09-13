ff42ca98ca2529739724dc933dc613fe20d966e1
Ajitkumar7338 <chimmalagiajitkumar971@gmail.com>
2024-09-13 16:11:04 +0530

    Add files via upload

--- main (2).c
+++ main (2).c
@@ -1,37 +1,14 @@
 #include<stdio.h>
-int add(int num1,int num2); 
+#include<stdlib.h>
+#include<string.h>
 int main(){
-  int num1,num2,sum,diff,prod,div;
-  printf("enter two numberd");
-  scanf("%d %d",&num1,&num2);
-  sum=add(num1,num2);
-  prod=num1*num2;
-  diff=num1-num2;
-  div=num1/num2;
-  printf("sum=%d\n",sum);
-  printf("prod=%d\n",prod);
-  printf("diff=%d\n",diff);
-  printf("div=%d\n",div);
+  char *name;
+  name = malloc(5);
+  name = realloc(name,20);
+  strcpy(name,"virat");
+  printf("%s\n",name);
+  free(name);
+  return 0;
 }
-int add(int a,int b)
-{
-  return a+b;
-}
-int diff(int a,int b)
-{
-  return a-b;
-}
-int prod(int a,int b)
-{
-   return a*b;
-}
-int div(int a,int b)
-{
-  return a/b;
-}
- 
   
-  
- 
-
 
